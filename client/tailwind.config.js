694451dcc25bf63244e816429b717f2337c91a90
Serhii Tenshi <job.tenshi@gmail.com>
2025-01-24 23:40:51 +0200

    config updates

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
+        colors: {
+            white: "#ffffff",
+            main: "#1E40AF",
+            darkblue: "#162345",
+            black: "#000000",
+            dark: "#6B7280",
+            gray: "#D1D5DB",
+            transperent: "#00000000",
+            light: "#F7F7F7",
+        },
         extend: {},
     },
     plugins: [],
