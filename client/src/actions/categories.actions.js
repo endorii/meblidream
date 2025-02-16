dddf1495b27fddcaaa263d46d612d86fc4ea3c2b
Serhii Tenshi <job.tenshi@gmail.com>
2025-02-16 22:42:34 +0200

    test actions

--- client/src/actions/categories.actions.js
+++ client/src/actions/categories.actions.js
@@ -2,10 +2,45 @@ import axios from "axios";
 
 export const getCategories = async () => {
     try {
-        const response = await axios.get(`http://localhost:3000/categories`);
+        const response = await axios.get(`http://localhost:5000/api/categories`);
+        return response.data.categories;
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+export const addCategories = async (
+    displayName,
+    pathName,
+    title,
+    subtitle,
+    description,
+    images
+) => {
+    try {
+        const response = await axios.post(`http://localhost:5000/api/categories`, {
+            displayName,
+            pathName,
+            filling: {
+                title,
+                subtitle,
+                description,
+            },
+            images,
+        });
+
         return response.data;
     } catch (e) {
-        console.log("Помилка при отриманні категорій:", e.message);
-        throw e;
+        console.log(e.response.data.message);
+    }
+};
+
+export const deleteCategory = async (categoryId) => {
+    try {
+        const response = await axios.delete(`http://localhost:5000/api/categories/${categoryId}`);
+
+        return response.data;
+    } catch (e) {
+        console.log(e.response.data.message);
     }
 };
--- client/src/actions/orders.actions.js
+++ client/src/actions/orders.actions.js
@@ -2,10 +2,34 @@ import axios from "axios";
 
 export const getOrders = async () => {
     try {
-        const response = await axios.get("http://localhost:3000/orders");
-        return response.data;
+        const response = await axios.get(`http://localhost:5000/api/orders`);
+        return response.data.orders;
     } catch (e) {
         console.log(e);
         throw e;
+    }
+};
+
+export const addOrder = async (name, phone, message) => {
+    try {
+        const response = await axios.post(`http://localhost:5000/api/orders`, {
+            name,
+            phone,
+            message,
+        });
+
+        return response.data;
+    } catch (e) {
+        console.log(e.response.data.message);
+    }
+};
+
+export const deleteOrder = async (orderId) => {
+    try {
+        const response = await axios.delete(`http://localhost:5000/api/orders/${orderId}`);
+
+        return response.data;
+    } catch (e) {
+        console.log(e.response.data.message);
     }
 };
