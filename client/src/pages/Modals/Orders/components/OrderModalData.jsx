42158b7ff35cabe2c4336c0c411d5245ede61c3a
Serhii Tenshi <job.tenshi@gmail.com>
2025-02-17 17:15:27 +0200

    Order ui optimization

---
+++ client/src/pages/Modals/Orders/components/OrderModalData.jsx
@@ -0,0 +1,57 @@
+import MainButton from "../../../ui/buttons/MainButton";
+
+import ArrowLeftIcon from "../../../../assets/svg/arrow-left.svg?react";
+import MailIcon from "../../../../assets/svg/mail.svg?react";
+import PhoneIcon from "../../../../assets/svg/phone.svg?react";
+import InstagramIcon from "../../../../assets/svg/instagram.svg?react";
+import TelegramIcon from "../../../../assets/svg/telegram.svg?react";
+import WhatsAppIcon from "../../../../assets/svg/whatsapp.svg?react";
+import FacebookIcon from "../../../../assets/svg/facebook.svg?react";
+const OrderModalData = ({ onClose }) => {
+    return (
+        <div className="flex flex-col justify-center items-center md:items-start text-center md:text-left sm:justify-between w-full ">
+            <MainButton
+                bonusStyles="flex items-center gap-[10px] group"
+                onClose={onClose}
+            >
+                <ArrowLeftIcon className="stroke-white w-[30px] group-hover:stroke-main" />
+                Повернутися назад
+            </MainButton>
+            <div className="text-white text-[34px] sm:text-[38px] lg:text-[48px] font-semibold w-[90%] mt-[20px] sm:mt-[10px]">
+                Давайте реалізуємо меблі вашої{" "}
+                <span className="text-lightmain font-bold underline">мрії</span>{" "}
+                разом
+            </div>
+            <ul className="flex flex-col gap-[10px] sm:gap-[20px] mt-[30px] w-full">
+                <li className="flex items-center gap-[10px] text-white text-[16px] sm:text-[20px] p-[20px] rounded-xl border border-transperent hover:border hover:border-main hover:bg-main/30 cursor-pointer transition duration-300 ease-in-out">
+                    <MailIcon className="stroke-lightmain w-[30px] " />
+                    meblidream@i.com
+                </li>
+                <li className="flex items-center gap-[10px] text-white text-[16px] sm:text-[20px] p-[20px] rounded-xl border border-transperent hover:border hover:border-main hover:bg-main/30 cursor-pointer transition duration-300 ease-in-out">
+                    <PhoneIcon className="fill-lightmain w-[30px] " />
+                    +38 097 00 00 000
+                </li>
+                <li className="flex items-center gap-[10px] text-white text-[16px] sm:text-[20px] p-[20px] rounded-xl border border-transperent hover:border hover:border-main hover:bg-main/30 cursor-pointer transition duration-300 ease-in-out">
+                    <PhoneIcon className="fill-lightmain w-[30px] " />
+                    +38 097 00 00 001
+                </li>
+            </ul>
+            <ul className="flex mt-[30px] xl:mt-[10%] gap-[5px]">
+                <li className="p-[15px] hover:bg-main/30 rounded-[50%] border border-transperent hover:border hover:border-main hover:bg-main/30 cursor-pointer transition duration-300 ease-in-out">
+                    <InstagramIcon className="fill-white w-[25px] " />
+                </li>
+                <li className="p-[15px] hover:bg-main/30 rounded-[50%] border border-transperent hover:border hover:border-main hover:bg-main/30 cursor-pointer transition duration-300 ease-in-out">
+                    <TelegramIcon className="fill-white w-[25px] " />
+                </li>
+                <li className="p-[15px] hover:bg-main/30 rounded-[50%] border border-transperent hover:border hover:border-main hover:bg-main/30 cursor-pointer transition duration-300 ease-in-out">
+                    <WhatsAppIcon className="fill-white w-[25px] " />
+                </li>
+                <li className="p-[15px] hover:bg-main/30 rounded-[50%] border border-transperent hover:border hover:border-main hover:bg-main/30 cursor-pointer transition duration-300 ease-in-out">
+                    <FacebookIcon className="fill-white w-[25px] " />
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default OrderModalData;
---
+++ client/src/pages/Modals/Orders/components/OrderModalForm.jsx
@@ -0,0 +1,129 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import MainButton from "../../../ui/buttons/MainButton";
+import { addOrder } from "../../../../actions/orders.actions";
+import { fetchOrders } from "../../../../store/slices/orders.slice";
+
+const OrderModalForm = ({ onClose }) => {
+    const dispatch = useDispatch();
+
+    const themes = ["Здійснити замовлення", "Отримати консультацію", "Інше..."];
+
+    const [activeTheme, setActiveTheme] = useState(themes[0]);
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [message, setMessage] = useState("");
+
+    return (
+        <div
+            className={`bg-white flex flex-col gap-[20px] h-full rounded-2xl p-[20px] md:p-[40px] w-full`}
+        >
+            <div className="flex flex-col gap-[10px] ">
+                <div className="text-[20px] font-semibold">
+                    Я зацікавлений ...
+                </div>
+
+                <ul className="flex flex-wrap gap-[10px] text-center">
+                    {themes.map((theme, i) => {
+                        return (
+                            <li
+                                key={i}
+                                className={`py-[10px] sm:py-[13px] px-[20px] border rounded-xl text-[15px] sm:text-[16px] cursor-pointer
+                            ${
+                                activeTheme === theme
+                                    ? "bg-mainbg text-white"
+                                    : "text-main hover:bg-mainbg hover:text-white"
+                            }`}
+                                onClick={() => {
+                                    setActiveTheme(theme);
+                                }}
+                            >
+                                {theme}
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+            <form
+                className="flex flex-col gap-[20px] mt-[10px] sm:mt-[30px] bg-white"
+                action=""
+            >
+                <div className="flex flex-col gap-[15px]">
+                    <div className="flex flex-col gap-[5px]">
+                        <label
+                            className="text-[14px] sm:text-[16px] font-semibold"
+                            htmlFor="orderName"
+                        >
+                            Ім&apos;я та прізвище:
+                        </label>
+                        <input
+                            className="p-[10px] border-b border-darkgray h-[50px] w-full outline-none text-[16px] md:text-[18px]"
+                            type="text"
+                            id="orderName"
+                            name="orderName"
+                            required
+                            value={name}
+                            onChange={(e) => {
+                                setName(e.target.value);
+                            }}
+                        />
+                    </div>
+
+                    <div className="flex flex-col gap-[10px]">
+                        <label
+                            className="text-[14px] sm:text-[16px] font-semibold"
+                            htmlFor="orderPhone"
+                        >
+                            Номер телефону:
+                        </label>
+                        <input
+                            className="p-[10px] border-b border-darkgray h-[50px] w-full outline-none text-[16px] md:text-[18px]"
+                            type="tel"
+                            id="orderPhone"
+                            name="orderPhone"
+                            pattern="[0-9]{10}"
+                            required
+                            value={phone}
+                            onChange={(e) => {
+                                setPhone(e.target.value);
+                            }}
+                        />
+                    </div>
+
+                    <div className="flex flex-col gap-[10px]">
+                        <label
+                            className="text-[14px] sm:text-[16px] font-semibold"
+                            htmlFor="orderMessage"
+                        >
+                            Повідомлення:
+                        </label>
+                        <textarea
+                            className="p-[10px] border-b border-darkgray h-[70px] outline-none text-[16px] md:text-[18px]"
+                            id="orderMessage"
+                            name="orderMessage"
+                            rows="4"
+                            required
+                            value={message}
+                            onChange={(e) => {
+                                setMessage(e.target.value);
+                            }}
+                        ></textarea>
+                    </div>
+                </div>
+                <MainButton
+                    bonusStyles=""
+                    onAction={async () => {
+                        await addOrder(activeTheme, name, phone, message);
+                        dispatch(fetchOrders());
+                        onClose();
+                    }}
+                    isDisabled={!activeTheme || !name || !phone || !message}
+                >
+                    Відправити
+                </MainButton>
+            </form>
+        </div>
+    );
+};
+
+export default OrderModalForm;
