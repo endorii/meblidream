246687a8761cbd39b013b82b361b95b522f98eef
Serhii Tenshi <job.tenshi@gmail.com>
2025-02-26 15:27:56 +0200

    auth actions

---
+++ client/src/actions/auth.actions.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { setUser } from "../store/slices/user.slice";
+import toast from "react-hot-toast";
+
+export const login = (login, password) => {
+    return async (dispatch) => {
+        try {
+            const response = await axios.post(`http://localhost:5000/api/auth/login/`, {
+                login,
+                password,
+            });
+            dispatch(setUser(response.data.user));
+            localStorage.setItem("accessToken", response.data.accessToken);
+            toast.success(response.data.message);
+
+            console.log(response.data);
+        } catch (e) {
+            toast.error(e.response.data.message);
+            console.log(e.response.data.message);
+        }
+    };
+};
+
+export const auth = () => {
+    return async (dispatch) => {
+        try {
+            const response = await axios.get("http://localhost:5000/api/auth/auth", {
+                headers: { Authorization: `Bearer ${localStorage.getItem("accessToken")}` },
+            });
+            dispatch(setUser(response.data.user));
+            localStorage.setItem("accessToken", response.data.accessToken);
+        } catch (e) {
+            console.log(e.response.data.message);
+
+            localStorage.removeItem("accessToken");
+        }
+    };
+};
