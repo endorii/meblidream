72cb79935ef67eb3439cf9c5550936a4a0b35ead
Serhii Tenshi <job.tenshi@gmail.com>
2025-02-26 15:27:19 +0200

    user slice in redux

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoriesSlice from "./slices/categories.slice";
 import ordersSlice from "./slices/orders.slice";
+import userSlice from "./slices/user.slice";
 
 const rootReducer = {
     categories: categoriesSlice,
     orders: ordersSlice,
+    user: userSlice,
 };
 
 const store = configureStore({
