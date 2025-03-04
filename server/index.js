d039deb36100a84ed07dbf6fab34798810c4d43a
Serhii Tenshi <job.tenshi@gmail.com>
2025-03-04 15:39:01 +0200

    images routes usage

--- server/index.js
+++ server/index.js
@@ -5,18 +5,23 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = config.get("PORT");
 const cors = require("cors");
+const path = require("path");
 
 const authRouter = require("../server/routes/auth.routes");
 const orderRoutes = require("./routes/order.routes");
 const categoryRoutes = require("./routes/category.routes");
+const uploadRoutes = require("./routes/upload.routes");
 
 app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
 
+app.use("/images", express.static(path.join(__dirname, "images")));
+
 app.use("/api/auth", authRouter);
 app.use("/api/", orderRoutes);
 app.use("/api/", categoryRoutes);
+app.use("/api/", uploadRoutes);
 
 const start = async () => {
     try {
