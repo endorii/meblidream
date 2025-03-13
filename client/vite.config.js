f79cd0fbbb32cbe29b96cb56773dfbe75e19c5cd
Serhii Tenshi <job.tenshi@gmail.com>
2025-03-13 17:28:58 +0200

    build

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,8 +2,22 @@ import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+    build: {
+        outDir: "../dist/client",
+        rollupOptions: {
+            output: {
+                // Окремо для зображень
+                assetFileNames: (assetInfo) => {
+                    if (/\.(png|jpe?g|gif|svg)$/.test(assetInfo.name)) {
+                        return "assets/images/[name].[hash][extname]";
+                    }
+                    // Для всіх інших файлів
+                    return "assets/[name].[hash][extname]";
+                },
+            },
+        },
+    },
     plugins: [react(), svgr()],
     server: {
         host: true,
